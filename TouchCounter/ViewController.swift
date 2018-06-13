e3a45913e3479d4ece60a80312bba866d955b3dd
Giuseppe Santaniello <giuseppesantaniello@yahoo.it>
2018-06-13 07:55:22 +0200

    Initial Commit

---
+++ TouchCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchCounter
+//
+//  Created by GSanta on 13/06/18.
+//  Copyright © 2018 GSanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
